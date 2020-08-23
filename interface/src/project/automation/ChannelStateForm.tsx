9aecff5d79d08ae4734d1bcbe35b2a069c4dfef2
Tichaona Hwandaza <tichaona@yahoo.com>
2020-08-23 09:39:36 -0500

    refactored hot time hour UI

--- interface/src/project/automation/ChannelStateForm.tsx
+++ interface/src/project/automation/ChannelStateForm.tsx
@@ -172,6 +172,39 @@ const ChannelStateForm = (props : ChannelStateRestControllerFormProps) => {
         return `${value}hr`;
       }
 
+      const formatHotTimeHour = (hotTimeHour: number) => {
+        const totalTimeSeconds = hotTimeHour * 3600;
+        const hours: number = Math.floor((hotTimeHour));
+        const minutes: number = Math.floor(((totalTimeSeconds - (hours*3600)) / 60));
+        const seconds: number = Math.floor(totalTimeSeconds - (hours*3600) - (minutes * 60));
+        
+        let hString: string = hours.toString();
+        if(hours < 10){hString = "0" + hours;}
+        if (hours == 0){
+          hString="";
+        }else{
+          hString= hString + "h ";
+        }
+
+        let mString: string = minutes.toString();
+        if(minutes < 10){mString = "0" + minutes;}
+        if (minutes == 0){
+          mString="";
+        }else{
+          mString= mString + "m ";
+        }
+
+        let sString: string = seconds.toString();
+        if(seconds < 10){sString = "0" + seconds;}
+        if (seconds == 0){
+          sString="";
+        }else{
+          sString= sString + "s";
+        }
+
+        return (hString + mString + sString);
+      }
+
     return (
       <ValidatorForm onSubmit={saveFormAndRestartSchedule}>
         <Typography variant="h6">{data.name} <SystemStateWebSocketController/></Typography>
@@ -318,15 +351,14 @@ const ChannelStateForm = (props : ChannelStateRestControllerFormProps) => {
         {!data.enableTimeSpan && data.randomize && (
         <div>
           <Typography id="discrete-slider" gutterBottom>
-          Hot Time
+          Hot Time: {formatHotTimeHour(data.schedule.hotTimeHour)} 
           </Typography>
           <Slider
             disabled={!data.randomize}
             defaultValue={data.schedule.hotTimeHour}
-            getAriaValueText={valuetext}
             aria-labelledby="discrete-slider-custom"
             step={0.0167}
-            valueLabelDisplay="auto"
+            valueLabelDisplay="off"
             marks={marks}
             min={0}
             max={4}
