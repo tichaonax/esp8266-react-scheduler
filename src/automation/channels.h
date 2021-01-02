6e410f978ca4bcab276981aa59718ff9abfe0d01
Tichaona Hwandaza <tichaona@yahoo.com>
2021-01-01 20:10:43 -0600

    settings for d1-mini testing

--- src/automation/channels.h
+++ src/automation/channels.h
@@ -44,24 +44,24 @@
 #define CHANNEL_THREE_CONTROL_PIN 13
 #define CHANNEL_THREE_REST_ENDPOINT_PATH "/rest/channelThreeState"  //restChannelEndPoint
 #define CHANNEL_THREE_SOCKET_PATH "/ws/channelThreeState"  // webSocketChannelEndPoint
-#define CHANNEL_THREE_DEFAULT_NAME "Inside Lights" //  defaultChannelName
+#define CHANNEL_THREE_DEFAULT_NAME "D-Mini Insides Lights" //  defaultChannelName
 #define CHANNEL_THREE_CONFIG_JSON_PATH "/config/channelThreeState.json"  // channelJsonConfigPath
 #define CHANNEL_THREE_HOME_ASSISTANT_ENTITY "inside_light"    // homeAssistantEntity
-#define CHANNEL_THREE_DEFAULT_CONTROL_RUN_EVERY 20.0f
+#define CHANNEL_THREE_DEFAULT_CONTROL_RUN_EVERY 10.0f
 #define CHANNEL_THREE_DEFAULT_CONTROL_OFF_AFTER 5.0f
-#define CHANNEL_THREE_DEFAULT_CONTROL_START_TIME_HOUR 19
+#define CHANNEL_THREE_DEFAULT_CONTROL_START_TIME_HOUR 6
 #define CHANNEL_THREE_DEFAULT_CONTROL_START_TIME_MINUTE 0
-#define CHANNEL_THREE_DEFAULT_CONTROL_END_TIME_HOUR 4
-#define CHANNEL_THREE_DEFAULT_CONTROL_END_TIME_MINUTE 30
+#define CHANNEL_THREE_DEFAULT_CONTROL_END_TIME_HOUR 5
+#define CHANNEL_THREE_DEFAULT_CONTROL_END_TIME_MINUTE 0
 #define CHANNEL_THREE_DEFAULT_ENABLED_STATE true
 #define CHANNEL_THREE_DEFAULT_ENABLE_TIME_SPAN_SCHEDULE false
 #define CHANNEL_THREE_DEFAULT_RANDOMIZE_SCHEDULE true
-#define CHANNEL_THREE_DEFAULT_SPAN_TIME 3.0f
+#define CHANNEL_THREE_DEFAULT_SPAN_TIME 13.0f
 
 #define CHANNEL_FOUR_CONTROL_PIN 14
 #define CHANNEL_FOUR_REST_ENDPOINT_PATH "/rest/channelFourState"  //restChannelEndPoint
 #define CHANNEL_FOUR_SOCKET_PATH "/ws/channelFourState"  // webSocketChannelEndPoint
-#define CHANNEL_FOUR_DEFAULT_NAME "Outside Lights" //  defaultChannelName
+#define CHANNEL_FOUR_DEFAULT_NAME "D-Mini Outside Lights" //  defaultChannelName
 #define CHANNEL_FOUR_CONFIG_JSON_PATH "/config/channelFourState.json"  // channelJsonConfigPath
 #define CHANNEL_FOUR_HOME_ASSISTANT_ENTITY "outside_light"    // homeAssistantEntity
 #define CHANNEL_FOUR_DEFAULT_CONTROL_RUN_EVERY 30.0f
