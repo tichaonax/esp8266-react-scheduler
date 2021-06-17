be26da0be20bb4909bdff63f0994d5d42cdd00a5
Tichaona Hwandaza <tichaona@yahoo.com>
2021-06-17 00:56:57 -0500

    changed defaults

--- src/automation/channels.h
+++ src/automation/channels.h
@@ -4,7 +4,7 @@
 #define CHANNEL_ONE_CONTROL_PIN 5
 #define CHANNEL_ONE_REST_ENDPOINT_PATH "/rest/channelOneState"  //restChannelEndPoint
 #define CHANNEL_ONE_SOCKET_PATH "/ws/channelOneState"  // webSocketChannelEndPoint
-#define CHANNEL_ONE_DEFAULT_NAME "Mobile Light" //  defaultChannelName
+#define CHANNEL_ONE_DEFAULT_NAME "Water Pump" //  defaultChannelName
 #define CHANNEL_ONE_CONFIG_JSON_PATH "/config/channelOneState.json"  // channelJsonConfigPath
 #define CHANNEL_ONE_HOME_ASSISTANT_ENTITY "mobile_light"
 #define CHANNEL_ONE_DEFAULT_CONTROL_RUN_EVERY 30.0f
@@ -39,28 +39,24 @@
 #define CHANNEL_TWO_DEFAULT_OVERRIDE_TIME 60.0f // override time in minutes
 #define CHANNEL_TWO_DEFAULT_ENABLE_MINIMUM_RUN_TIME_SCHEDULE false
 
-#ifdef SINILINK
-  // onboard relay control pin = 4
-  #define CHANNEL_THREE_CONTROL_PIN 4
-#else
-    #define CHANNEL_THREE_CONTROL_PIN 13
-#endif
+// MAC F4:CF:A2:D7:7B:65
+#define CHANNEL_THREE_CONTROL_PIN 5
 #define CHANNEL_THREE_REST_ENDPOINT_PATH "/rest/channelThreeState"  //restChannelEndPoint
 #define CHANNEL_THREE_SOCKET_PATH "/ws/channelThreeState"  // webSocketChannelEndPoint
-#define CHANNEL_THREE_DEFAULT_NAME "Main Workshop Lights" //  defaultChannelName
+#define CHANNEL_THREE_DEFAULT_NAME "Moblie Light" //  defaultChannelName
 #define CHANNEL_THREE_CONFIG_JSON_PATH "/config/channelThreeState.json"  // channelJsonConfigPath
-#define CHANNEL_THREE_HOME_ASSISTANT_ENTITY "channel_three"    // homeAssistantEntity
+#define CHANNEL_THREE_HOME_ASSISTANT_ENTITY "mobile_light"    // homeAssistantEntity
 #define CHANNEL_THREE_DEFAULT_CONTROL_RUN_EVERY 30.0f
 #define CHANNEL_THREE_DEFAULT_CONTROL_OFF_AFTER 10.0f
-#define CHANNEL_THREE_DEFAULT_CONTROL_START_TIME_HOUR 1
+#define CHANNEL_THREE_DEFAULT_CONTROL_START_TIME_HOUR 19
 #define CHANNEL_THREE_DEFAULT_CONTROL_START_TIME_MINUTE 0
-#define CHANNEL_THREE_DEFAULT_CONTROL_END_TIME_HOUR 19
+#define CHANNEL_THREE_DEFAULT_CONTROL_END_TIME_HOUR 4
 #define CHANNEL_THREE_DEFAULT_CONTROL_END_TIME_MINUTE 30
 #define CHANNEL_THREE_DEFAULT_ENABLED_STATE true
-#define CHANNEL_THREE_DEFAULT_ENABLE_TIME_SPAN_SCHEDULE true
-#define CHANNEL_THREE_DEFAULT_RANDOMIZE_SCHEDULE false
+#define CHANNEL_THREE_DEFAULT_ENABLE_TIME_SPAN_SCHEDULE false
+#define CHANNEL_THREE_DEFAULT_RANDOMIZE_SCHEDULE true
 #define CHANNEL_THREE_DEFAULT_SPAN_TIME 3.0f
-#define CHANNEL_THREE_DEFAULT_OVERRIDE_TIME 120.0f // override time in minutes
+#define CHANNEL_THREE_DEFAULT_OVERRIDE_TIME 60.0f // override time in minutes
 #define CHANNEL_THREE_DEFAULT_ENABLE_MINIMUM_RUN_TIME_SCHEDULE false
 
 #define CHANNEL_FOUR_CONTROL_PIN 14
