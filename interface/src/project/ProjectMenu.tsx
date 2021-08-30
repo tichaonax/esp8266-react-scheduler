528acb1379de1b83a7348d388f5873be082cdc4c
Tichaona Hwandaza <tichaona@yahoo.com>
2021-08-30 07:47:29 -0500

    cleaned up unused code and disabled info to freem memory

--- interface/src/project/ProjectMenu.tsx
+++ interface/src/project/ProjectMenu.tsx
@@ -4,7 +4,6 @@ import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 import {List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import BlurCircularIcon from '@material-ui/icons/BlurCircular';
-//import SettingsRemoteIcon from '@material-ui/icons/SettingsRemote';
 
 import { PROJECT_PATH } from '../api';
 
@@ -14,12 +13,6 @@ class ProjectMenu extends Component<RouteComponentProps> {
     const path = this.props.match.url;
     return (
       <List>
-     {/*    <ListItem to={`/${PROJECT_PATH}/demo/`} selected={path.startsWith(`/${PROJECT_PATH}/demo/`)} button component={Link}>
-          <ListItemIcon>
-            <SettingsRemoteIcon />
-          </ListItemIcon>
-          <ListItemText primary="Demo Project" />
-        </ListItem> */}
         <ListItem to={`/${PROJECT_PATH}/status/`} selected={path.startsWith(`/${PROJECT_PATH}/status/`)} button component={Link}>
           <ListItemIcon>
             <BlurCircularIcon />
