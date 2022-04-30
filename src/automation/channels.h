3dc5b9c69b0f036be9dfc127c1c64c053092c7c9
Tichaona Hwandaza <tichaona@yahoo.com>
2022-04-30 08:28:20 -0500

    upgrade

--- src/automation/channels.h
+++ src/automation/channels.h
@@ -34,7 +34,7 @@
 
 #define CHANNEL_ONE_REST_ENDPOINT_PATH "/rest/channelOneState"  //restChannelEndPoint
 #define CHANNEL_ONE_SOCKET_PATH "/ws/channelOneState"  // webSocketChannelEndPoint
-#define CHANNEL_ONE_DEFAULT_NAME "Water Pump X" //  defaultChannelName
+#define CHANNEL_ONE_DEFAULT_NAME "Water Pump" //  defaultChannelName
 #define CHANNEL_ONE_CONFIG_JSON_PATH "/config/channelOneState.json"  // channelJsonConfigPath
 #define CHANNEL_ONE_HOME_ASSISTANT_ENTITY "ch1"
 #define CHANNEL_ONE_DEFAULT_CONTROL_RUN_EVERY 15.0f
