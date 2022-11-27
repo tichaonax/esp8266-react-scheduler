487a2f45005163a29d34ca9d69506e5939283603
Tichaona Hwandaza <tichaonax@gmail.com>
2022-11-26 18:58:33 -0600

    add ui support for days of week the schedule is enabled

---
+++ interface/src/project/automation/channels/rest/channelStateForm/selectWeekDays.tsx
@@ -0,0 +1,100 @@
+import { withStyles } from "@material-ui/core/styles";
+import ToggleButton from "@mui/lab/ToggleButton";
+import ToggleButtonGroup from "@mui/lab/ToggleButtonGroup";
+import { blueGrey, yellow } from '@mui/material/colors';
+import { ChannelState } from "../../../redux/types/channel";
+
+const DAYS = [
+  {
+    key: "sunday",
+    label: "S"
+  },
+  {
+    key: "monday",
+    label: "M"
+  },
+  {
+    key: "tuesday",
+    label: "T"
+  },
+  {
+    key: "wednesday",
+    label: "W"
+  },
+  {
+    key: "thursday",
+    label: "T"
+  },
+  {
+    key: "friday",
+    label: "F"
+  },
+  {
+    key: "saturday",
+    label: "S"
+  }
+];
+
+const StyledToggleButtonGroup = withStyles((theme) => ({
+  grouped: {
+    margin: theme.spacing(2),
+    padding: theme.spacing(0, 1),
+    "&:not(:first-child)": {
+      border: "1px solid",
+      borderColor: blueGrey[800],
+      borderRadius: "50%"
+    },
+    "&:first-child": {
+      border: "1px solid",
+      borderColor: blueGrey[800],
+      borderRadius: "50%"
+    }
+  }
+}))(ToggleButtonGroup);
+
+const StyledToggle = withStyles({
+  root: {
+    color: yellow[500],
+    "&$selected": {
+      color: "white",
+      background: "#346beb"
+    },
+    "&:hover": {
+      borderColor: "#346beb",
+      background: "#346beb"
+    },
+    "&:hover$selected": {
+      borderColor: "#346beb",
+      background: "#346beb"
+    },
+    minWidth: 32,
+    maxWidth: 32,
+    height: 32,
+    textTransform: "unset",
+    fontSize: "0.75rem"
+  },
+  selected: {}
+})(ToggleButton);
+
+export function selectWeekDays(
+    data: ChannelState,
+    handleChannelSelectWeekDaysValueChange: (value: number[]) => void)
+    {
+  return (
+    <>
+    <p>Days of the week</p>
+      <StyledToggleButtonGroup
+        size="small"
+        arial-label="Days of the week"
+        value={data.schedule.weekDays}
+        onChange={(_event, value) => handleChannelSelectWeekDaysValueChange(value)}
+      >
+        {DAYS.map((day, index) => (
+          <StyledToggle key={day.key} value={index} aria-label={day.key}>
+            {day.label}
+          </StyledToggle>
+        ))}
+      </StyledToggleButtonGroup>
+    </>
+  );
+}
